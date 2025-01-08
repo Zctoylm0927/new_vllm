af733fd858ceff6716ed3cfe7224cb59e814fa14
WindOctober <windocotber@gmail.com>
2025-01-08 22:37:24 +0800

    ignore test

--- test.py
+++ test.py
@@ -5,19 +5,21 @@ import json
 
 with open('request.json', 'r') as file:
     data = json.load(file)
-    
+
+
 def send_questions_per_second(data):
     for t, question_info in data.items():
         if question_info:
             for question in question_info:
                 question = question["sentence"]
                 print(f"sending question: {question}")
-                
+
                 ttft = get_ttft(question)
                 tpot = get_tpot(question)
                 print(f"ttft: {ttft:.3f} , tpot: {tpot} tokens/s")
 
         time.sleep(1)
-        
+
+
 if __name__ == "__main__":
     send_questions_per_second(data)
